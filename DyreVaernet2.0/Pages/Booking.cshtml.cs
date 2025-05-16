027e7b1f0f3d96bae7359f486e8f14cad1922c6d
Omar Yasser Soboh <oms001@edu.zealand.dk>
2025-05-16 11:39:18 +0200

    Pages added Blog, Booking and Adopt

--- DyreVaernet2.0/Pages/Booking.cshtml.cs
+++ DyreVaernet2.0/Pages/Booking.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace DyreVaernet2._0
+namespace DyreVaernet2._0.Pages
 {
-    public class BlogModel : PageModel
+    public class BookingModel : PageModel
     {
         public void OnGet()
         {
