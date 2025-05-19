54c7f9b2e2a820ecd9c72224b4756fc24939383e
HelinSaraBaran <sara501d@edu.zealand.dk>
2025-05-19 09:19:59 +0200

    adopt

--- DyreVaernet2.0/Pages/Adopt.cshtml.cs
+++ DyreVaernet2.0/Pages/Adopt.cshtml.cs
@@ -1,12 +1,22 @@
-using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Domain.Interfaces;
+using Domain.Models;
+using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DyreVaernet2._0.Pages
 {
+    // PageModel til Adopt-siden
     public class AdoptModel : PageModel
     {
+        // Liste over dyr, som vises i Razor-siden
+        public List<Animal> Animals { get; set; }
+
         public void OnGet()
         {
+            // Opretter repository og henter dyrene fra JSON-filen
+            IAnimalRepository repository = new AnimalRepository();
+            Animals = repository.GetAll();
         }
     }
 }
