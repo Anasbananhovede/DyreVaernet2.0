51c72216847ec52f39e667323ef9c8918f09176d
Anasbananhovede <anas24092003978@gmail.com>
2025-05-20 12:29:39 +0200

    ændrede navne på dyr

--- DyreVaernet2.0/IRepo/AnimalRepo.cs
+++ DyreVaernet2.0/IRepo/AnimalRepo.cs
@@ -7,14 +7,14 @@ namespace DyreVaernet2._0.IRepo
         List<Animal> _animals = new List<Animal>();
         public AnimalRepo() // int roomID, string name, string description, int capacity, List<string> equipment
         {
-            _animals.Add(new Animal(101, "Alpha", "description 1"));
-            _animals.Add(new Animal(102, "Bravo", "description 2"));
-            _animals.Add(new Animal(103, "Charlie", "description 3"));
-            _animals.Add(new Animal(104, "Delta", "description 4"));
-            _animals.Add(new Animal(105, "Echo", "description 5"));
-            _animals.Add(new Animal(106, "Foxtrot", "description 6"));
-            _animals.Add(new Animal(107, "Golf", "description 7"));
-            _animals.Add(new Animal(108, "Hotel", "description 8"));
+            _animals.Add(new Animal(101, "martin", "description 1"));
+            _animals.Add(new Animal(102, "Bruno", "description 2"));
+            _animals.Add(new Animal(103, "sofus", "description 3"));
+            _animals.Add(new Animal(104, "selma", "description 4"));
+            _animals.Add(new Animal(105, "enola", "description 5"));
+            _animals.Add(new Animal(106, "fister", "description 6"));
+            _animals.Add(new Animal(107, "Gurt", "description 7"));
+            _animals.Add(new Animal(108, "rexsil", "description 8"));
         }
         public List<Animal> GetAll()
         {
